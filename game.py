570b77a86b236298827b1b2e7bc7acc865fd15d0
NoraNader <noranader1372@gmail.com>
2024-05-16 17:15:22 +0300

    test

--- game.py
+++ game.py
@@ -1,3 +1,32 @@
-#game file!
-print("hello")
-#hi
+import pygame
+import random
+import os
+
+
+pygame.init()
+pygame.display.set_caption("Kings and Pigs")
+screen_width= 1000
+screen_hight=1000
+window= pygame.display.set_mode((screen_width,screen_hight))
+
+FPS= 60
+king_velocity= 5
+
+def main(window):
+    clock=pygame.time.Clock()
+    run= True
+    while run:
+        clock.tick(FPS)
+
+
+
+
+
+
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+        pygame.display.update()
+    pygame.quit()
