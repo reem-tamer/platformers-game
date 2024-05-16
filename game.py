b753e3f2e10f151919bc4423bb37c67597e8f00f
reemt <reemtamerkhalil@gmail.com>
2024-05-16 22:49:16 +0300

    push

--- game.py
+++ game.py
@@ -18,16 +18,29 @@ king_velocity= 5
 #setting the images
 background= pygame.image.load("background.png")
 bar=pygame.image.load("bar.png")
+
+class Bar(pygame.sprite.Sprite):
+    def __init__(self,x,y):
+        super().__init__()
+        self.image = pygame.image.load("bar.png")
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
 #def main(window):
 clock=pygame.time.Clock()
 run= True
+
 while run:
+
     clock.tick(FPS)
     window.fill((0,0,0))
     window.blit(background,(0,0))
-    window.blit(bar,(400,320))
-    window.blit(bar,(250,380))
-
+    window.blit(bar,(400,320))  #1
+    window.blit(bar,(250,380))  #2
+    window.blit(bar, (100, 300)) #3
+    window.blit(bar, (570, 185)) #4
+    window.blit(bar, (570, 280)) #5
+    window.blit(bar, (400, 230)) #6
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
