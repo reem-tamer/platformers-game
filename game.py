476b238d83b87f5115a5a0bc12418ac96bab29b2
reemt <reemtamerkhalil@gmail.com>
2024-05-17 16:10:07 +0300

    push

--- game.py
+++ game.py
@@ -20,7 +20,7 @@ background= pygame.image.load("background.png")
 bar=pygame.image.load("bar.png")
 
 
-
+#create platform bars main class
 class Platform(pygame.sprite.Sprite):
     def __init__(self, x, y, image_file):
         super().__init__()
@@ -28,7 +28,7 @@ class Platform(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.topleft = (x, y)
 
-# Create platforms
+# Create platforms using sprite groups, putting each bar in its position
 platforms = pygame.sprite.Group()
 platforms.add(Platform(400, 320, 'bar.png'))
 platforms.add(Platform(250, 380, 'bar.png'))
@@ -42,8 +42,8 @@ platforms.add(Platform(400, 230, 'bar.png'))
 
 
 
+#main game loop
 
-#def main(window):
 clock=pygame.time.Clock()
 run= True
 
@@ -53,8 +53,25 @@ while run:
     window.fill((0,0,0))
     window.blit(background,(0,0))
 
-
+#"blitting" the bars but instead of using blit we use draw
+    # since it is a function related to sprite groups
     platforms.draw(window)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   # to exit the game
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
