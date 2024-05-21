7d115661054fcd483768461003aefdaae4c53b01
reemt <reemtamerkhalil@gmail.com>
2024-05-21 17:04:29 +0300

    push

--- game.py
+++ game.py
@@ -31,7 +31,7 @@ fall_img = pygame.image.load('Fall (78x58).png').convert_alpha()
 pig_idle= pygame.image.load('Idle_pig.png').convert_alpha()
 diamond_img= pygame.image.load("diamond.png").convert_alpha()
 
-
+#hellooo noooraaaa
 #create platform bars main class
 class Platform(pygame.sprite.Sprite):
     def __init__(self, x, y, image_file):
